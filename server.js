8620d38b00927753d6fe32380bf8fe18f893ed5e
André Coelho <archanjoandre@yahoo.com.br>
2024-05-25 18:56:46 -0300

    Refatorando código

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
+const CONTENT_TYPE = 'Content-Type';
+const TEXT_PLAIN = 'text/plain';
+const RESPONSE_MESSAGE = "Hello World!";
+const SERVER_RUNNING_MESSAGE = `O servidor está sendo executado em http://${HOSTNAME}:${PORT}/`;
 
-http.createServer((req, res) => {
+const requestHandler = (req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'text/plain'
+        [CONTENT_TYPE]: TEXT_PLAIN
     });
-    res.end("Hello World!");
-}).listen(port, hostname, () => {
-    console.log(`O servidor está sendo executado em htpp://${hostname}:${port}/`);
+    res.end(RESPONSE_MESSAGE);
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(SERVER_RUNNING_MESSAGE);
 });
